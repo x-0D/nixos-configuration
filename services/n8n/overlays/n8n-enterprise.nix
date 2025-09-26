b11bd6e28451a597af887d2de9ebd88644cf91aa
Nikita Bragin <x.0d@ya.ru>
2025-09-26 22:29:42 +0000

    update hashes

--- services/n8n/overlays/n8n-enterprise.nix
+++ services/n8n/overlays/n8n-enterprise.nix
@@ -7,13 +7,13 @@ final: prev: {
       owner = "Abdulazizzn";
       repo = "n8n-enterprise-unlocked";
       rev = "master";
-      hash = "sha256-0000000000000000000000000000000000000000000000000000";
+      hash = "sha256-WZSqFphQMucN5cMMPkFKt3c+Y/WTCrxwGNayRg0FlyI=";
     };
     
     pnpmDeps = final.pnpm_10.fetchDeps {
       inherit (finalAttrs) pname version src;
       fetcherVersion = 2;
-      hash = "sha256-0000000000000000000000000000000000000000000000000000";
+      hash = "sha256-0kI7YL0d20IOYvMYgJ8TBHFksyviBcpujYIGl6GMmd8=";
     };
   });
 }
