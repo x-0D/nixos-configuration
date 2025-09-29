d2ae749b68665eff9a063c9daf0ceaa6472be4fe
Nikita Bragin <x.0d@ya.ru>
2025-09-29 20:34:45 +0000

    update n8n to 1.112.6

--- services/n8n/overlays/n8n-enterprise.nix
+++ services/n8n/overlays/n8n-enterprise.nix
@@ -1,23 +1,25 @@
 # overlays/n8n-enterprise.nix
 final: prev: {
   n8n = prev.n8n.overrideAttrs (finalAttrs: previousAttrs: {
-    version = "1.112.0";
-    
+    version = "1.112.6";
+
     src = final.fetchFromGitHub {
-      owner = "Abdulazizzn";
+      owner = "x-0D";
       repo = "n8n-enterprise-unlocked";
-      rev = "master";
-      hash = "sha256-WZSqFphQMucN5cMMPkFKt3c+Y/WTCrxwGNayRg0FlyI=";
+      rev = "1.112.6";
+      # 1.112.0-hash = "sha256-WZSqFphQMucN5cMMPkFKt3c+Y/WTCrxwGNayRg0FlyI=";
+      hash = "sha256-z7nFmRRYEj6AHeTwuCEH/N9Oc5tFlEzq5ag20XyD81o=";
     };
-    
+
+    # You already have pnpmDeps, keep that
     pnpmDeps = final.pnpm_10.fetchDeps {
       inherit (finalAttrs) pname version src;
       fetcherVersion = 2;
+      # 1.112.0-hash = "sha256-0kI7YL0d20IOYvMYgJ8TBHFksyviBcpujYIGl6GMmd8=";
       hash = "sha256-0kI7YL0d20IOYvMYgJ8TBHFksyviBcpujYIGl6GMmd8=";
     };
 
     passthru = previousAttrs.passthru // {
-      # Remove the updateScript to disable automatic updates
       updateScript = "/dev/null";
     };
   });
